4300a7a4d233011bf2d66753bcbe6abeae5cd9f8
Wolf Rieder <wolf-rieder@t-online.de>
2022-10-23 17:37:32 +0200

    update processing and add sphinx docs

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. HTTP Header Analyzer documentation master file, created by
+   sphinx-quickstart on Sun Oct 23 17:32:44 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to HTTP Header Analyzer's documentation!
+================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/transformer/test.py
+++ src/transformer/test.py
@@ -1,38 +1,19 @@
 import os
 import sys
+
 # import pandas as pd
 import ray
 
 # import modin.pandas as pd
 import pandas as pd
+
 # import yaml
 # from pandarallel import pandarallel
 import swifter
 from rapidfuzz import process
 import numpy as np
-
-# pandarallel.initialize(progress_bar=True)
-
-
-# params = yaml.safe_load(open('params.yaml'))
-
-# print(params['browsers'])
-#
-# print(params['browsers']['chrome'])
-
-# print(pd.read_json(f'../../data/raw/{params["browsers"]["chrome"]}/'
-#                    f'{params["file_name"]["date"]}/http.0.json'))
-
-# print(sys.argv)
-
-# dict_categories = {"test": sys.argv[3]}
-# number = sys.argv[3].split('.')[1]
-
-# with open(f"data/raw/{sys.argv[2]}/{sys.argv[3]}.json", "w") as f:
-#     orjson.dump(dict_categories, f)
-
-# test = pd.read_json('../../data/raw/chrome/08_12_2022/http.0.json.gzip',
-#                     compression='gzip')
+from alive_progress import alive_bar
+from collections import Counter
 
 
 def new_fuzzy_string_matching_for_column(col_name, col_values):
@@ -108,21 +89,61 @@ def variance_per_column_2(column):
     return [column, trackers, non_trackers]
 
 
-def concise_information(col_list, classification):
-    indices = set()
+def concise_information(col_list, classification, dataset):
+    indices = list()
 
     for col in col_list:
-        indices.update(
-            data[
-                (data[col].notnull()) & (data["tracker"] == classification)
+        indices.append(
+            dataset[
+                (dataset[col].notnull()) & (dataset["tracker"] == classification)
             ].index.tolist()
         )
 
     return indices
 
 
-def test_new_categories_update(element):
-    categories = data[element].astype("category").cat.categories.values.tolist()
+def find_cols_to_combine(information_table):
+    only_non_trackers = information_table[
+        information_table["ratio"] <= 10
+    ].header_name.values.tolist()
+    only_trackers = information_table[
+        information_table["ratio2"] <= 10
+    ].header_name.values.tolist()
+    return only_non_trackers, only_trackers
+
+
+def concise_information_wrapper(dataset, table):
+    only_non_tracker_cols, only_tracker_cols = find_cols_to_combine(table)
+
+    dataset["comb_col_non_tracker"] = 0
+    dataset["comb_col_tracker"] = 0
+
+    col_tracker = dict(
+        Counter(
+            list(
+                np.concatenate(concise_information(only_tracker_cols, 1, dataset)).flat
+            )
+        )
+    )
+    col_non_tracker = dict(
+        Counter(
+            list(
+                np.concatenate(
+                    concise_information(only_non_tracker_cols, 0, dataset)
+                ).flat
+            )
+        )
+    )
+
+    for key, value in col_tracker.items():
+        dataset.at[key, "comb_col_tracker"] = value
+
+    for key, value in col_non_tracker.items():
+        dataset.at[key, "comb_col_non_tracker"] = value
+
+
+def test_new_categories_update(element, dataset):
+    categories = dataset[element].astype("category").cat.categories.values.tolist()
     try:
         np.array(categories, dtype="int64")
         return {element: "Int64"}
@@ -130,189 +151,194 @@ def test_new_categories_update(element):
         return None
 
 
-def impute_value(element, classification):
-    if element in list_of_integer_cols:
-        data.loc[data['tracker'] == classification, element] = data.loc[data['tracker'] == classification, element]\
-            .fillna(data[data['tracker']==classification][element].median())
+def create_categories_list(dataset):
+    dtype_list = {i: "category" for i in dataset.columns.values[:-2]}
+    current_columns = dataset.columns.values[6:-2].tolist()
+    int64_columns = [
+        test_new_categories_update(element, dataset) for element in current_columns
+    ]
 
-    if element in list_of_categorical_cols:
-        data.loc[data['tracker'] == classification, element] = data.loc[data['tracker'] == classification, element]\
-            .fillna(data[data['tracker']==classification][element].mode().iloc[0])
+    int64_columns = list(filter(lambda x: type(x) is dict, int64_columns))
+    int64_columns = {k: v for d in int64_columns for k, v in d.items()}
+
+    dtype_list.update(int64_columns)
+    del dtype_list["query"]
+    del dtype_list["protocol"]
+    return dtype_list
 
 
-print("Read data")
-data = pd.read_parquet(
-    "../../data/processed/chrome/08_12_2022/train_set_01.parquet.gzip"
-)
+def create_summary_table(dataset):
+    number_of_elements_reduced = np.array(
+        [variance_per_column_2(column) for column in dataset.iloc[:, 4:-2].columns]
+    )
+    summary_table_2 = pd.DataFrame(
+        number_of_elements_reduced, columns=["header_name", "trackers", "non_trackers"]
+    )
+    summary_table_2["trackers"] = summary_table_2["trackers"].astype("Int32")
+    summary_table_2["non_trackers"] = summary_table_2["non_trackers"].astype("float32")
+    summary_table_2["ratio"] = (
+        summary_table_2["trackers"] / summary_table_2["non_trackers"]
+    ) * 100
+    summary_table_2["ratio2"] = (
+        summary_table_2["non_trackers"] / summary_table_2["trackers"]
+    ) * 100
+    return summary_table_2
 
-data_test = pd.read_parquet(
-    "../../data/processed/chrome/08_12_2022/test_set_01.parquet.gzip"
-)
 
-print("Count number of headers per message")
-data["header_count"] = data.iloc[:, 6:-1].notnull().sum(axis=1)
-data_test["header_count"] = data.iloc[:, 6:-1].notnull().sum(axis=1)
+if __name__ == "__main__":
+    with alive_bar(100, force_tty=True, manual=True, title="Data Processing") as bar:
+        bar.text('Read-in data')
+        data = pd.read_parquet(
+            "../../data/processed/chrome/08_12_2022/train_set_01.parquet.gzip"
+        )
 
-print("Remove empty columns")
-empty_columns = [col for col in data if data[col].isnull().all() == True]
-data.drop(empty_columns, axis=1, inplace=True)
-data_test.drop(empty_columns, axis=1, inplace=True)
+        data_test = pd.read_parquet(
+            "../../data/processed/chrome/08_12_2022/test_set_01.parquet.gzip"
+        )
+        bar(0.05)
 
-print("Remove duplicated observations")
-data = data[~data.iloc[:, 6:-2].duplicated(keep="first")].reset_index(drop=True)
+        bar.text('Count number of headers per message')
+        data["header_count"] = data.iloc[:, 6:-1].notnull().sum(axis=1)
+        bar(0.1)
+        data_test["header_count"] = data_test.iloc[:, 6:-1].notnull().sum(axis=1)
+        bar(0.15)
 
-print("Fuzzy match")
-data_column_values = data.columns.values[6:-2].tolist()
-match = [
-    new_fuzzy_string_matching_for_column(j, data_column_values[i + 1:])
-    for i, j in enumerate(data_column_values)
-    if i != len(data_column_values) - 1
-]
+        bar.text('Remove empty columns')
+        empty_columns = [col for col in data if data[col].isnull().all() == True]
+        data.drop(empty_columns, axis=1, inplace=True)
+        data_test.drop(empty_columns, axis=1, inplace=True)
+        bar(0.2)
 
-match2 = pd.concat(match, ignore_index=True)
-del match
+        # Is that even necessary? Check papers; also in context of HTTP traffic.
+        # bar.text("Remove duplicated observations")
+        # data = data[~data.iloc[:, 6:-2].duplicated(keep="first")].reset_index(drop=True)
 
-print("Find fuzzy matches with similar columns")
+        bar.text('Fuzzy match')
+        data_column_values = data.columns.values[6:-2].tolist()
+        match = [
+            new_fuzzy_string_matching_for_column(j, data_column_values[i + 1:])
+            for i, j in enumerate(data_column_values)
+            if i != len(data_column_values) - 1
+        ]
 
-result = [
-    find_cols_with_similar_values(col, col2)
-    for col, col2 in zip(match2["fuzzy_match"], match2["col_name"])
-]
+        match2 = pd.concat(match, ignore_index=True)
+        del match
+        bar(0.3)
 
-print("Reset index")
-data.reset_index(drop=True, inplace=True)
-data_test.reset_index(drop=True, inplace=True)
+        bar.text('Find fuzzy matches with similar columns')
 
-similar_values = [
-    select_similar_columns(col[0], col[1]) for col in result if col is not None
-]
-similar_values = pd.concat(similar_values, ignore_index=True)
-similar_values.apply(
-    lambda x: merge_similar_columns2(x["fuzzy_match"], x["col_name"]), axis=1
-)
+        result = [
+            find_cols_with_similar_values(col, col2)
+            for col, col2 in zip(match2["fuzzy_match"], match2["col_name"])
+        ]
+        bar(0.4)
 
-similar_values_test = pd.concat(similar_values, ignore_index=True)
-similar_values_test.apply(
-    lambda x: merge_similar_columns2_test(x["fuzzy_match"], x["col_name"]), axis=1
-)
+        bar.text('Reset index')
+        data.reset_index(drop=True, inplace=True)
+        data_test.reset_index(drop=True, inplace=True)
 
-del match2
+        similar_values = [
+            select_similar_columns(col[0], col[1]) for col in result if col is not None
+        ]
 
-columns_to_remove = list(set(similar_values.fuzzy_match.values.tolist()))
-data.drop(columns_to_remove, axis=1, inplace=True)
-data_test.drop(columns_to_remove, axis=1, inplace=True)
+        similar_values_test = pd.concat(similar_values, ignore_index=True)
+        similar_values = pd.concat(similar_values, ignore_index=True)
+        similar_values.apply(
+            lambda x: merge_similar_columns2(x["fuzzy_match"], x["col_name"]), axis=1
+        )
 
-del result
-del similar_values
-del similar_values_test
-del columns_to_remove
+        similar_values_test.apply(
+            lambda x: merge_similar_columns2_test(x["fuzzy_match"], x["col_name"]),
+            axis=1,
+        )
 
-print("Data types")
-columns_as_category = {i: "category" for i in data.columns.values[:-2]}
-column_test = data.columns.values[6:-2].tolist()
-braze2 = [test_new_categories_update(element) for element in column_test]
+        del match2
 
-braze2 = list(filter(lambda x: type(x) is dict, braze2))
-braze2 = {k: v for d in braze2 for k, v in d.items()}
+        columns_to_remove = list(set(similar_values.fuzzy_match.values.tolist()))
+        data.drop(columns_to_remove, axis=1, inplace=True)
+        data_test.drop(columns_to_remove, axis=1, inplace=True)
 
-columns_as_category.update(braze2)
-del columns_as_category["query"]
-del columns_as_category["protocol"]
-data.drop(["protocol", "query"], axis=1, inplace=True)
-data_test.drop(["protocol", "query"], axis=1, inplace=True)
-data = data.astype(columns_as_category)
-data_test = data_test.astype(columns_as_category)
+        del result
+        del similar_values
+        del similar_values_test
+        del columns_to_remove
 
-print("Remove columns with 1.0 na_ratio")
-number_of_elements = np.array(
-    [reduced_variance_per_column(column) for column in data.iloc[:, 4:-2].columns]
-)
-summary_table = pd.DataFrame(
-    number_of_elements, columns=["header_name", "unique_values", "na_ratio"]
-)
-summary_table["unique_values"] = summary_table["unique_values"].astype("Int32")
-summary_table["na_ratio"] = summary_table["na_ratio"].astype("float32")
-remove_headers_with_one_na_ratio = summary_table[
-    summary_table["na_ratio"] == 1
-].header_name.values.tolist()
-remove_headers_with_one_value = summary_table[
-    (summary_table["unique_values"] <= 1) & (summary_table["na_ratio"] != 1)
-].header_name.values.tolist()
+        bar(0.6)
 
-print("Drop columns")
+        bar.text('Data types')
+        list_of_dtypes = create_categories_list(data)
+        list_of_dtypes_test = create_categories_list(data_test)
+        data.drop(["protocol", "query"], axis=1, inplace=True)
+        data_test.drop(["protocol", "query"], axis=1, inplace=True)
+        data = data.astype(list_of_dtypes)
+        data_test = data_test.astype(list_of_dtypes_test)
 
-data.drop(remove_headers_with_one_na_ratio, axis=1, inplace=True)
-data.drop(remove_headers_with_one_value, axis=1, inplace=True)
+        bar(0.7)
 
-data_test.drop(remove_headers_with_one_na_ratio, axis=1, inplace=True)
-data_test.drop(remove_headers_with_one_value, axis=1, inplace=True)
+        bar.text('Find columns with na_ratio of 1')
+        number_of_elements = np.array(
+            [
+                reduced_variance_per_column(column)
+                for column in data.iloc[:, 4:-2].columns
+            ]
+        )
+        summary_table = pd.DataFrame(
+            number_of_elements, columns=["header_name", "unique_values", "na_ratio"]
+        )
+        summary_table["unique_values"] = summary_table["unique_values"].astype("Int32")
+        summary_table["na_ratio"] = summary_table["na_ratio"].astype("float32")
+        remove_headers_with_one_na_ratio = summary_table[
+            summary_table["na_ratio"] == 1
+        ].header_name.values.tolist()
+        remove_headers_with_one_value = summary_table[
+            (summary_table["unique_values"] <= 1) & (summary_table["na_ratio"] != 1)
+        ].header_name.values.tolist()
 
-del remove_headers_with_one_na_ratio
-del number_of_elements
-del summary_table
+        bar(0.8)
 
-print("Combine columns")
-number_of_elements_reduced = np.array(
-    [variance_per_column_2(column) for column in data.iloc[:, 4:-2].columns]
-)
-summary_table = pd.DataFrame(
-    number_of_elements_reduced, columns=["header_name", "trackers", "non_trackers"]
-)
-summary_table["trackers"] = summary_table["trackers"].astype("Int32")
-summary_table["non_trackers"] = summary_table["non_trackers"].astype("float32")
-summary_table["ratio"] = (
-    summary_table["trackers"] / summary_table["non_trackers"]
-) * 100
-summary_table["ratio2"] = (
-    summary_table["non_trackers"] / summary_table["trackers"]
-) * 100
+        bar.text('Combine columns')
+        summary_table2 = create_summary_table(data)
 
-only_non_tracker_col = summary_table[
-    summary_table["ratio"] <= 10
-].header_name.values.tolist()
-only_tracker_col = summary_table[
-    summary_table["ratio2"] <= 10
-].header_name.values.tolist()
+        concise_information_wrapper(data, summary_table2)
+        concise_information_wrapper(data_test, summary_table2)
 
-data["comb_col_non_tracker"] = 0
-data["comb_col_tracker"] = 0
+        bar(0.9)
 
-for idx in concise_information(only_tracker_col, 1):
-    data.at[idx, "comb_col_tracker"] = 1
+        bar.text('Drop columns')
 
-for idx in concise_information(only_non_tracker_col, 0):
-    data.at[idx, "comb_col_non_tracker"] = 1
+        data.drop(remove_headers_with_one_na_ratio, axis=1, inplace=True)
+        data.drop(remove_headers_with_one_value, axis=1, inplace=True)
 
-data.drop(only_non_tracker_col, axis=1, inplace=True)
-data.drop(only_tracker_col, axis=1, inplace=True)
+        data_test.drop(remove_headers_with_one_na_ratio, axis=1, inplace=True)
+        data_test.drop(remove_headers_with_one_value, axis=1, inplace=True)
 
-print("Imputation")
+        del remove_headers_with_one_na_ratio
+        del number_of_elements
+        del summary_table
 
-list_of_categorical_cols = list(data.iloc[:, 4:-4].select_dtypes('category').columns.values.tolist())
-list_of_integer_cols = list(data.iloc[:, 4:-4].select_dtypes('Int64').columns.values.tolist())
+        bar(0.95)
 
-number_of_trackers = len(data[data['tracker'] == 1])
-number_of_non_trackers = len(data[data['tracker'] == 0])
-summary_table['ratio_tracker'] = summary_table['trackers'] / number_of_trackers
-summary_table['ratio_non_tracker'] = summary_table['non_trackers'] / number_of_non_trackers
+        bar.text('Remove remaining combined columns')
+        summary_table2 = create_summary_table(data)
+        only_non_tracker_col, only_tracker_col = find_cols_to_combine(summary_table2)
 
-impute_col_list_t = summary_table[summary_table['ratio_tracker'] > 0.4].header_name.values.tolist()
-impute_col_list_nt = summary_table[summary_table['ratio_non_tracker'] > 0.4].header_name.values.tolist()
+        data.drop(only_non_tracker_col, axis=1, inplace=True)
+        data.drop(only_tracker_col, axis=1, inplace=True)
 
-for header in impute_col_list_t:
-    impute_value(header, 0)
+        data_test.drop(only_non_tracker_col, axis=1, inplace=True)
+        data_test.drop(only_tracker_col, axis=1, inplace=True)
 
-for header in impute_col_list_t:
-    impute_value(header, 1)
+        bar(0.99)
 
-for header in impute_col_list_nt:
-    impute_value(header, 1)
+        bar.text('Write data to parquet.gzip')
 
-for header in impute_col_list_nt:
-    impute_value(header, 0)
+        data.to_parquet(
+            "../../data/processed/chrome/08_12_2022/train_set_01_processed.parquet.gzip",
+            compression="gzip",
+        )
 
-data.to_parquet(
-    "../../data/processed/chrome/08_12_2022/train_set_01_processed.parquet.gzip", compression="gzip"
-)
-
-print("end")
+        data_test.to_parquet(
+            "../../data/processed/chrome/08_12_2022/test_set_01_processed.parquet.gzip",
+            compression="gzip",
+        )
+        bar(1)
