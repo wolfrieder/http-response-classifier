55deed7d4a470d6d03a9bcecb012517ce2ee4ccd
Wolf Rieder <wolf-rieder@t-online.de>
2022-11-04 09:16:03 +0100

    update dvc and modeling

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,6 @@
+import os
+import warnings
+
 import yaml
 from sklearn.dummy import DummyClassifier
 import pandas as pd
@@ -24,33 +27,45 @@ import seaborn as sns
 from scipy.stats import kstest, shapiro, probplot
 import category_encoders as ce
 import mlflow
+import os
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
+    warnings.filterwarnings('ignore')
     with open('../../params.yaml', 'rb') as f:
         params = yaml.load(f, yaml.FullLoader)
 
-    # mlflow.set_tracking_uri(params[])
-    print(params['ml_flow']['MLFLOW_TRACKING_URI'])
-    # train_data = pd.read_parquet(
-    #     "../../data/processed/chrome/08_12_2022/train_set_01_featurized.parquet.gzip"
-    # )
-    # test_data = pd.read_parquet(
-    #     "../../data/processed/chrome/08_12_2022/test_set_01_featurized.parquet.gzip"
-    # )
-    #
-    # X_train, y_train = train_data.iloc[:, :-1], train_data[["tracker"]]
-    # X_test, y_test = test_data.iloc[:, :-1], test_data[["tracker"]]
-    #
-    # # models
-    # rf_model = RandomForestClassifier(
-    #     n_estimators=100,
-    #     n_jobs=-1,
-    #     random_state=10,
-    #     criterion="log_loss",
-    #     max_features=None,
-    # )
-    #
+    mlflow.set_tracking_uri(params['ml_flow']['MLFLOW_TRACKING_URI'])
+    os.environ['MLFLOW_TRACKING_USERNAME'] = params['ml_flow']['MLFLOW_TRACKING_USERNAME']
+    os.environ['MLFLOW_TRACKING_PASSWORD'] = params['ml_flow']['MLFLOW_TRACKING_PASSWORD']
+
+    # mlflow.sklearn.autolog()
+
+    train_data = pd.read_parquet(
+        "../../data/processed/chrome/08_12_2022/train_set_01_featurized.parquet.gzip"
+    )
+    test_data = pd.read_parquet(
+        "../../data/processed/chrome/08_12_2022/test_set_01_featurized.parquet.gzip"
+    )
+
+    X_train, y_train = train_data.iloc[:, :-1], train_data[["tracker"]]
+    X_test, y_test = test_data.iloc[:, :-1], test_data[["tracker"]]
+
+    # with mlflow.start_run():
+
+    # models
+    rf_model = RandomForestClassifier(
+        n_estimators=100,
+        n_jobs=-1,
+        random_state=10,
+        criterion="log_loss",
+        max_features=None,
+    )
+
     # knn_model = KNeighborsClassifier(n_jobs=-1)
     # dt_model = DecisionTreeClassifier()
     # gbm_model = GradientBoostingClassifier()
@@ -58,25 +73,27 @@ if __name__ == "__main__":
     # xgb_model = xgb.XGBClassifier()
     # lgb_model = lgb.LGBMClassifier()
     # cat_model = CatBoostClassifier()
-    #
-    # # pipeline
-    # numeric_transformer = Pipeline(steps=[("scaler", FunctionTransformer(np.log1p))])
-    #
-    # preprocessor = ColumnTransformer(
-    #     transformers=[
-    #         ("num", numeric_transformer, ["content-length"]),
-    #         ("cat", ce.WOEEncoder(), selector(dtype_include="category")),
-    #     ]
-    # )
-    #
-    # clf = Pipeline(steps=[("preprocessor", preprocessor), ("classifier", rf_model)])
-    #
-    # clf.fit(X_train, y_train["tracker"].to_numpy())
-    # y_pred = clf.predict(X_test)
-    # print(
-    #     "Model accuracy score : {0:0.4f}".format(metrics.accuracy_score(y_test, y_pred))
-    # )
-    #
-    # clf_probs = clf.predict_proba(X_test)
-    # score = metrics.log_loss(y_test, clf_probs)
-    # print("Model log-loss score : {0:0.4f}".format(score))
+
+    # pipeline
+    numeric_transformer = Pipeline(steps=[("scaler", FunctionTransformer(np.log1p))])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transformer, ["content-length"]),
+            ("cat", ce.WOEEncoder(), selector(dtype_include="category")),
+        ]
+    )
+
+    clf = Pipeline(steps=[("preprocessor", preprocessor), ("classifier", rf_model)])
+
+    clf.fit(X_train, y_train["tracker"].to_numpy())
+    y_pred = clf.predict(X_test)
+    print(
+        "Model accuracy score : {0:0.4f}".format(metrics.accuracy_score(y_test, y_pred))
+    )
+
+    clf_probs = clf.predict_proba(X_test)
+    score = metrics.log_loss(y_test, clf_probs)
+    print("Model log-loss score : {0:0.4f}".format(score))
+
+        # mlflow.end_run()
